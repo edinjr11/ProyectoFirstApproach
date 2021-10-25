57bc811908cfdd3bc7abf7e69dcd5407e91314dd
edinjr11 <eycecr11@gmail.com>
2021-10-24 21:07:59 -0600

    All the changes made on the creating a blog module

--- app.js
+++ app.js
@@ -15,9 +15,34 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
 
+app.get("/",function(req,res){
+  res.render("home",{thisIsFromApp:homeStartingContent, posts: posts});
+});
 
+app.get("/contact",function(req,res){
+  res.render("contact",{thisIsContact:contactContent});
+});
 
+app.get("/about",function(req,res){
+  res.render("about",{thisIsAbout:aboutContent});
+});
+
+app.get("/compose",function(req,res){
+  res.render("compose");
+});
+
+app.post("/compose",function(req,res){
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.postBody
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
+});
 
 
 
